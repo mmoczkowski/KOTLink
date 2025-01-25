5f954bf4eae6396cf076b1aa9d8c2377c96447a0
mmoczkowski <milosz.moczkowski@gmail.com>
2025-01-25 19:04:54 +0100

    Designate MavUnsupportedMessage as a data class

--- core/src/main/kotlin/com/mmoczkowski/mavlink/MavUnsupportedMessage.kt
+++ core/src/main/kotlin/com/mmoczkowski/mavlink/MavUnsupportedMessage.kt
@@ -1,5 +1,17 @@
 package com.mmoczkowski.mavlink
 
-class MavUnsupportedMessage(private val content: ByteArray) : MavLinkMessage {
+data class MavUnsupportedMessage(private val content: ByteArray) : MavLinkMessage {
     override fun toBytes(): ByteArray = content
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MavUnsupportedMessage
+
+        return content.contentEquals(other.content)
+    }
+
+    override fun hashCode(): Int {
+        return content.contentHashCode()
+    }
 }
